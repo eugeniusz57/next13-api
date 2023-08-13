3651e089da4ef0f303277a36e6157e5e8765053d
Eugeniusz <104994034+eugeniusz57@users.noreply.github.com>
2023-08-13 11:42:56 +0200

    use client component in server, use zustand

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,15 @@
-import Link from "next/link";
+import { Navigation } from "./Navigation";
 
-type Props = {};
+const navItems = [
+  { label: "Home", href: "/" },
+  { label: "Blog", href: "/blog" },
+  { label: "About", href: "/about" },
+];
 
-const Header = (props: Props) => {
+const Header = () => {
   return (
     <header>
-      <Link href="/">Home</Link>
-      <Link href="blog">Blog</Link>
-      <Link href="/about">About</Link>
+      <Navigation navLinks={navItems} />
     </header>
   );
 };
